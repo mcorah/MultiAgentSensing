72c89d34649e38641afb463e2554b8ad1664c735
Micah Corah <micahcorah@gmail.com>
2018-03-14 18:58:43 -0400

    mixtures working up through sampling

--- gaussian_test.jl
+++ gaussian_test.jl
@@ -1,6 +1,9 @@
 using SubmodularMaximization
 using PyPlot
 
+num_sample = 1000
+limits = [0 1; 0 1]
+
 function evaluate_pdf(p, name)
   println("Evaluating $name")
 
@@ -8,9 +11,19 @@ function evaluate_pdf(p, name)
   @show pdf(p, [0.5, 0.5])
 
   figure()
-  visualize_pdf(g)
+  @time visualize_pdf(p)
   title("$name")
+
+  println("Sampling PDF")
+  @time samples = hcat(map(x->sample_reject(p, limits), 1:num_sample)...)
+
+  scatter(samples[1,:]', samples[2,:]', color = "k", marker = (5, 2, 0))
 end
 
-g = Gaussian([0.5, 0.5], diagm([0.2, 1.0]))
+g = Gaussian([0.5, 0.5], diagm([0.01, 0.1]))
 evaluate_pdf(g, "Gaussian")
+
+bimodal_mixture = GaussianMixture([1.0, 1.5],
+                                  [Gaussian([0.2, 0.5], diagm([0.001, 0.1])),
+                                   Gaussian([0.8, 0.5], diagm([0.001, 0.1]))])
+evaluate_pdf(bimodal_mixture, "Bimodal Mixture")
--- probabilistic_coverage.jl
+++ probabilistic_coverage.jl
@@ -44,22 +44,36 @@ end
 # Environment model
 ###################
 
-export Gaussian, GaussianMixture, sample_pdf, pdf, visualize_pdf
+export Gaussian, GaussianMixture, sample_pdf, pdf, visualize_pdf, dim,
+in_limits, sample_reject
+
+pdf_coefficient(covariance) = sqrt((2*pi)^size(covariance,1) * det(covariance))
 
 type Gaussian
   mean::Array{Float64,1}
   covariance::Array{Float64,2}
+  chol_covariance # result of cholesky decomposition
+  inv_covariance # result of inverse
+  det_covariance::Float64 # result of determinant
+  pdf_coefficient::Float64 # result of pdf_coefficient
+
+  function Gaussian(mean, covariance)
+    new(mean, covariance, chol(covariance), inv(covariance), det(covariance),
+        pdf_coefficient(covariance))
+  end
 end
 
 type GaussianMixture
   weights::Array{Float64,1}
-  components::Gaussian
+  components::Array{Gaussian,1}
 end
 
-function sample_pdf(g::Gaussian)
-  U = chol(g.covariance)
+dim(g::Gaussian) = length(g.mean)
 
-  g.mean + (randn(1,size(g.covariance,1)) * U)
+dim(gm::GaussianMixture) = dim(gm.components[1])
+
+function sample_pdf(g::Gaussian)
+  g.mean + (randn(1,size(g.covariance,1)) * g.chol_covariance)'
 end
 
 function sample_pdf(gm::GaussianMixture)
@@ -68,26 +82,43 @@ function sample_pdf(gm::GaussianMixture)
   sample_pdf(sample(gm.components, Weights(normalized_weights)))
 end
 
+in_limits(x, limits) = all(map(1:length(x)) do ii
+                             limits[ii,1] <= x[ii] && x[ii] <= limits[ii,2]
+                           end)
+
+function sample_reject(p, limits)
+  ret = zeros(dim(p), dim(p))
+  found_valid = false
+
+  while ~found_valid
+    ret = sample_pdf(p)
+    found_valid = in_limits(ret, limits)
+  end
+
+  ret
+end
+
+
 function pdf{T <: Number}(g::Gaussian, x::Array{T,1})
   d = x - g.mean
 
-  exp(- d' * inv(g.covariance) * d / 2)[1] / sqrt((2*pi)^length(x)*det(g.covariance))
+  exp(- d' * g.inv_covariance * d / 2)[1] * g.pdf_coefficient
 end
 
 function pdf{T <: Number}(gm::GaussianMixture, x::Array{T,1})
   reduce(0.0, zip(gm.weights, gm.components)) do value, w_c
-    value + w_c[1] * pdf(w_c[2])
+    value + w_c[1] * pdf(w_c[2], x)
   end
 end
 
-function visualize_pdf(fun, limits = [0 1; 0 1], n = 1000)
+function visualize_pdf(fun; limits = [0 1; 0 1], n = 1000, cmap = "viridis")
   xlim = limits[1,:]
   ylim = limits[2,:]
 
   density = [pdf(fun, [x,y]) for x in linspace(xlim[1], xlim[2], n),
                                  y in linspace(ylim[1], ylim[2], n)]
 
-  imshow(density', cmap= "BuPu", vmin=minimum(density), vmax=maximum(density[:]),
+  imshow(density', cmap=cmap, vmin=minimum(density), vmax=maximum(density[:]),
          extent=[xlim[1], xlim[2], ylim[1], ylim[2]],
          interpolation="nearest", origin="lower")
 end
