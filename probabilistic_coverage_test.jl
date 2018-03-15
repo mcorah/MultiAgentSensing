0ae8f0c5cc7a05458c85eaea196531e85cc55727
Micah Corah <micahcorah@gmail.com>
2018-03-15 12:56:36 -0400

    initial test code for probabilistic sensors

---
+++ probabilistic_coverage_test.jl
@@ -0,0 +1,86 @@
+using PyPlot
+using SubmodularMaximization
+
+########
+# Params
+########
+
+num_events = 500
+
+num_agents = 5
+num_sensors = 6
+nominal_area = 0.5
+
+max_success_probability = 0.9
+sensor_radius = sqrt(nominal_area / (num_agents * pi))
+station_radius = 2 * sensor_radius
+
+#############
+# environment
+#############
+
+figure()
+events = generate_events(num_events)
+
+
+########
+# agents
+########
+
+agent_specification = ProbabilisticAgentSpecification(max_success_probability,
+                                                      sensor_radius,
+                                                      station_radius,
+                                                      num_sensors)
+
+agents = generate_agents(agent_specification, num_agents)
+f(x) = mean_detection_probability(x, events)
+problem = PartitionProblem(f, agents)
+
+
+####################
+# visualize scenario
+####################
+
+visualize_pdf(standard_mixture())
+visualize_events(events)
+colors = generate_colors(agents)
+visualize_agents(agents, colors)
+xlim([0, 1])
+ylim([0, 1])
+title("Scenario")
+
+#############
+# evaluateion
+#############
+function evaluate_solver(solver, name)
+  println("$name solver running")
+  @time solution = solver(problem)
+
+  figure()
+  xlim([0, 1])
+  ylim([0, 1])
+  colors = generate_colors(agents)
+  visualize_agents(agents, colors)
+  visualize_solution(problem, solution, colors)
+  visualize_events(events)
+
+  coverage = solution.value
+
+  title("$name Solver ($coverage)")
+
+  @show coverage
+end
+
+# evaluate basic solvers
+#evaluate_solver(solve_myopic, "Myopic")
+#evaluate_solver(solve_random, "Random")
+evaluate_solver(solve_sequential, "Sequential")
+
+# evaluate partition solvers
+for num_partitions in [2, 4, 8]
+  solve_n(p) = solve_n_partitions(num_partitions, p)
+  #evaluate_solver(solve_n, "Partition-$num_partitions")
+end
+
+@show mean_weight(problem)
+@show total_weight(problem)
---
+++ probabilistic_sensor_test.jl
@@ -0,0 +1,22 @@
+using SubmodularMaximization
+
+discretization = 1000
+
+num_agents = 20
+num_sensors = 6
+nominal_area = 1.0
+
+max_success_probability = 0.9
+sensor_radius = sqrt(nominal_area / (num_agents * pi))
+station_radius = 10 * sensor_radius
+
+sensor = ProbabilisticSensor([0.5, 0.5], sensor_radius, max_success_probability)
+
+density = [detection_probability(sensor, [x,y])
+           for x in linspace(0, 1, discretization),
+           y in linspace(0, 1, discretization)]
+
+figure()
+imshow(density', cmap="viridis", vmin=minimum(density), vmax=maximum(density[:]),
+       extent=[0, 1, 0, 1], interpolation="nearest", origin="lower")
+colorbar()
