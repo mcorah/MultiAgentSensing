0853d6b7c0a3e8d47a3e8b6a5eccb7904af2c2b8
Micah Corah <micahcorah@gmail.com>
2019-12-23 18:09:55 -0500

    a single robot example using mcts

---
+++ example/target_tracking/single_robot_example.jl
@@ -0,0 +1,65 @@
+using SubmodularMaximization
+using POMDPs
+using MCTS
+using PyPlot
+
+steps = 100
+grid_size = 10
+horizon = 2
+iterations = 100
+
+grid = Grid(grid_size, grid_size)
+sensor = RangingSensor(0.5^2, 0.1^2)
+
+robot_state = random_state(grid)
+target_state = random_state(grid)
+
+target_states = Array{State}(undef, steps)
+target_states[1] = target_state
+
+robot_states = Array{State}(undef, steps)
+robot_states[1] = robot_state
+
+histogram_filter = Filter(grid)
+
+solver = generate_solver(horizon, n_iterations = iterations)
+
+plot_state_space(grid)
+xlim([0, grid_size+1])
+ylim([0, grid_size+1])
+
+for ii = 2:steps
+  println("Step ", ii)
+
+  # Before the target moves and the robot receives a measurement, execute robot
+  # dynamics
+  mdp = SingleRobotTargetTrackingProblem(grid, sensor, horizon,
+                                         [histogram_filter])
+  policy = solve(solver, mdp)
+  global robot_state = action(policy, MDPState(robot_state))
+  robot_states[ii] = robot_state
+
+  # Then update the target and sample the observation
+  global target_state = target_dynamics(grid, target_states[ii-1])
+  target_states[ii] = target_state
+
+  range_observation = generate_observation(sensor, robot_state, target_state)
+
+  # After updating states, update the filter (in place for now)
+  process_update!(histogram_filter, transition_matrix(grid))
+  measurement_update!(histogram_filter, robot_state, get_states(grid), sensor,
+                      range_observation)
+
+  plots=[]
+  append!(plots, plot_trajectory(robot_states[1:ii], color=:blue))
+  #append!(plots, plot_states(robot_states), color=:blue)
+  append!(plots, plot_observation(robot_state, range_observation, color=:blue))
+  append!(plots, plot_trajectory(target_states[1:ii]))
+  append!(plots, visualize_filter(histogram_filter))
+
+  readline()
+
+  if ii < steps
+    foreach(x->x.remove(), plots)
+  end
+end
