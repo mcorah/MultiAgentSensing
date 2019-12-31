2ab89381ba8003ad469f92b673cdef77db81b97d
Micah Corah <micahcorah@gmail.com>
2019-12-31 18:14:10 -0500

    CMTS tree visualization and notebook

---
+++ example/target_tracking/visualize_mcts_tree.jl
@@ -0,0 +1,26 @@
+using SubmodularMaximization
+using POMDPs
+using MCTS
+using D3Trees
+
+grid_size = 10
+iterations = 10
+horizon = 2
+
+grid = Grid(grid_size, grid_size)
+sensor = RangingSensor(0.5^2, 0.1^2)
+
+@show robot_state = random_state(grid)
+
+histogram_filter = Filter(grid)
+
+problem = SingleRobotTargetTrackingProblem(grid, sensor, horizon,
+                                           [histogram_filter])
+
+solver = generate_solver(horizon, n_iterations = iterations,
+                         enable_tree_vis=true)
+policy = solve(solver, problem)
+
+a, info = action_info(policy, MDPState(robot_state))
+
+D3Tree(info[:tree])
