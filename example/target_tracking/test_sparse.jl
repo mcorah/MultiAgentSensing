c6911d9076ab19c139e0510e31273c7878c75be6
Micah Corah <micahcorah@gmail.com>
2020-01-21 20:00:18 -0500

    updating the sparse filters test

--- example/target_tracking/test_sparse.jl
+++ example/target_tracking/test_sparse.jl
@@ -1,6 +1,8 @@
 # Some simple tests to verify the sparse filtering code
 
 using SubmodularMaximization
+using Histograms
+using SparseArrays
 
 g = Grid(10, 10)
 
@@ -8,6 +10,26 @@ thresh = 1e-3
 
 sensor = RangingSensor()
 
+robot_state = (2,2)
 target_state = (5,5)
 
-histogram_filter = SparseFilter(g, target_state, threshold=thresh)
+target_filter = SparseFilter(g, target_state, threshold=thresh)
+
+@assert nnz(get_values(target_filter)) == 1
+
+@assert get_states(target_filter) == [target_state]
+
+process_update!(target_filter, transition_matrix(g))
+
+@assert nnz(get_values(target_filter)) == 5
+@assert length(get_states(g, target_filter)) == 5
+
+
+range_observation = generate_observation(g, sensor, robot_state,
+                                         target_state)
+measurement_update!(target_filter, robot_state,
+                    get_states(g, target_filter), sensor, g,
+                    range_observation)
+
+@assert nnz(get_values(target_filter)) == 5
+@assert length(get_states(g, target_filter)) == 5
