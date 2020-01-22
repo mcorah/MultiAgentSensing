144303278019bc5ba857eadf6e9855fdf645ec8c
Micah Corah <micahcorah@gmail.com>
2020-01-21 19:52:16 -0500

    changes to support SparseFilters based on sparse vectors

--- src/target_tracking/filtering.jl
+++ src/target_tracking/filtering.jl
@@ -23,26 +23,18 @@ get_states(g::Grid, _::AnyFilter) = get_states(g)
 
 get_states(::Grid, f::SparseFilter) = get_states(f)
 function get_states(f::SparseFilter)
-  data = get_data(f)
+  vec = get_values(f)
+  states = State[]
 
-  col::Int64 = 1
-
-  map(enumerate(rowvals(data))) do (ii, row)
-    # Advance the column number until the next contains values past
-    # the current(ii) index
-    while col < size(data, 2) && data.colptr[col+1] <= ii
-      col += 1
-    end
-    (row, col)
-  end
+  # Use the existing machinery to produce the appropriate indices
+  shape = CartesianIndices(size(f))
+  map(x->shape[x].I, vec.nzind)
 end
 
 # Compute cartesian indices in sparse arrays
-index_to_state(f::SparseFilter, ind) = index_to_state(get_data(f), ind)
-function index_to_state(data::AbstractSparseMatrix, ind)
-  row = rowvals(data)[ind]
-  col = findlast(x->x<=ind, data.colptr)
-  (row, col)
+function index_to_state(f::SparseFilter, ind)
+  shape = CartesianIndices(size(f))
+  shape[ind].I
 end
 
 # Constructor with known initial state
@@ -53,8 +45,10 @@ function Filter(g::Grid, initial_state::State)
   Filter((1:g.width, 1:g.height), data)
 end
 function SparseFilter(g::Grid, initial_state::State; kwargs...)
-  data = spzeros(dims(g)...)
-  data[initial_state...] = 1
+  data = spzeros(prod(dims(g)))
+
+  idx = LinearIndices(dims(g))
+  data[idx[initial_state...]] = 1
 
   SparseFilter((1:g.width, 1:g.height), data; kwargs...)
 end
@@ -66,11 +60,12 @@ function sample_state(grid::Grid, prior::Filter; rng=Random.GLOBAL_RNG)
   index_to_state(grid, ind)
 end
 function sample_state(grid::Grid, prior::SparseFilter; rng=Random.GLOBAL_RNG)
-  vals = nonzeros(get_data(prior))
+  vec = get_values(prior)
 
-  @views ind = sample(rng, 1:length(vals), Weights(vals))
+  # returns the linear index of the sampled state
+  ind = sample(rng, vec.nzind, Weights(vec.nzval))
 
-  # Here we go off our existing knowledge of the state representation...
+  # Turn this linear index into a proper state
   index_to_state(prior, ind)
 end
 
@@ -86,10 +81,11 @@ function process_update!(prior::Filter, transition_matrix)
   prior
 end
 function process_update!(prior::SparseFilter, transition_matrix)
+  # Note that the values and buffer are vectors that represent the flattened
+  # state grid
+  mul!(get_buffer(prior), transition_matrix, get_values(prior))
 
-  prod = transition_matrix * get_data(prior)[:]
-
-  prior.data = reshape(prod, size(prior))
+  swap_buffer!(prior)
 
   prior
 end
@@ -106,8 +102,24 @@ end
 #
 # Note: likelihoods should match the positions of the non-zeros for sparse
 # matrices
-function measurement_update!(prior::AnyFilter, likelihoods::Vector{Float64})
+function measurement_update!(prior::Filter, likelihoods::Vector{Float64})
   vals = get_values(prior)
+
+  # update belief in place
+  @inbounds @simd for ii in 1:length(vals)
+    vals[ii] *= likelihoods[ii]
+  end
+
+  # normalize
+  s = sum(vals)
+  @inbounds @simd for ii in 1:length(vals)
+    vals[ii] /= s
+  end
+
+  prior
+end
+function measurement_update!(prior::SparseFilter, likelihoods::Vector{Float64})
+  vals = get_values(prior).nzval
 
   # update belief in place
   @inbounds @simd for ii in 1:length(vals)
