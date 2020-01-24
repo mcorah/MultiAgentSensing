aa5d77c797442e6a633c48f80c2300fa9dbe73b6
Micah Corah <micahcorah@gmail.com>
2020-01-24 13:24:47 -0500

    updating robot and target plotting

--- src/target_tracking/visualization.jl
+++ src/target_tracking/visualization.jl
@@ -1,10 +1,12 @@
 using PyPlot
 using LinearAlgebra
 
-export plot_state_space, plot_states, plot_trajectory, plot_robot,
-  plot_observation, visualize_filter, visualize_filters
+export plot_state_space, plot_quadrotor, plot_target, plot_states,
+  plot_trajectory, plot_robot, plot_observation, visualize_filter,
+  visualize_filters
 
-const object_scale = 9^2
+const object_scale = 13^2
+const default_linewidth = 2.0
 
 function plot_state_space(g::Grid; color=:k)
   states = get_states(g)
@@ -12,6 +14,57 @@ function plot_state_space(g::Grid; color=:k)
   ys = [x[2] for x in states]
 
   scatter(xs, ys, color=color)
+end
+
+# Plots a visualization of a quadrotor
+rotate(theta) = [cos(theta) -sin(theta); sin(theta) cos(theta)]
+function circle(p; radius=1, scale=1, dth=0.1)
+  scale*hcat(map(x->p+[radius*cos(x);radius*sin(x)], 0:dth:(2*pi)+dth)...)
+end
+plot_quadrotor(p::State; kwargs...) = plot_quadrotor([p...]; kwargs...)
+function plot_quadrotor(p::Vector; scale=0.20,
+                        color="blue",
+                        linewidth=default_linewidth,
+                        theta=pi/6,
+                        alpha=1.0)
+  ret = []
+
+  rotor_radius = 0.5
+
+  rot = rotate(theta)
+
+  circle_ps = rot*scale*[[1;0] [0;1] [-1;0] [0;-1]]
+
+  p3 = 0.0
+  if length(p) == 3
+    p3 = p[3]
+  end
+
+  p2 = p[1:2]
+
+  # Plot X shape
+  l1 = [p2+scale*rot[:,1] p2-scale*rot[:,1]]
+  l2 = [p2+scale*rot[:,2] p2-scale*rot[:,2]]
+  append!(ret, plot(l1[1,:][:], l1[2,:][:], p3*ones(2), color=color, linewidth=linewidth,
+                  alpha=alpha))
+  append!(ret, plot(l2[1,:][:], l2[2,:][:], p3*ones(2), color=color, linewidth=linewidth,
+                  alpha = alpha))
+
+  # Plot and fill circles
+  for ii = 1:size(circle_ps,2)
+    c = circle(p2+circle_ps[:,ii], radius=scale*rotor_radius)
+    append!(ret, plot(c[1,:][:], c[2,:][:], p3*ones(size(c,2)), color=color,
+                    linestyle="--", linewidth=0.5linewidth, alpha=alpha))
+    append!(ret, fill(c[1,:][:], c[2,:][:], p3*ones(size(c,2)), color=:black,
+                    alpha=0.3alpha))
+  end
+
+  ret
+end
+
+function plot_target(p; color="red")
+  [scatter(p[1], p[2], color=color, marker="^", edgecolors=:k,
+           s=object_scale)]
 end
 
 # When a robot or target crosses a boundary of the grid, that creates a
@@ -38,16 +91,18 @@ function wrap_discontinuities(states)
   ranges
 end
 
-function plot_states(states::Array{State}; kwargs...)
+function plot_states(states::Array{State}; linewidth=default_linewidth,
+                     kwargs...)
   vcat(map(wrap_discontinuities(states)) do range
     xs = [x[1] for x in range]
     ys = [x[2] for x in range]
 
-    plot(xs, ys; kwargs...)
+    plot(xs, ys; linewidth=linewidth, kwargs...)
   end...)
 end
 
-function plot_trajectory(states; color=:red, kwargs...)
+function plot_trajectory(states; color=:red, linewidth=default_linewidth,
+                         kwargs...)
 
   first = states[1]
   last = states[end]
@@ -62,7 +117,7 @@ function plot_trajectory(states; color=:red, kwargs...)
   range_plots = map(wrap_discontinuities(states)) do range
     xs = [x[1] for x in range]
     ys = [x[2] for x in range]
-    plot(xs, ys; color=color, kwargs...)
+    plot(xs, ys; color=color, linewidth=default_linewidth, kwargs...)
   end
 
   # return values are arrays of plot objects, concatenate
