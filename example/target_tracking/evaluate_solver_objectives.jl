e6500787d941195c2043b41191f6167c17eb6354
Micah Corah <micahcorah@gmail.com>
2020-02-06 17:52:51 -0500

    rewrite to solver script

--- example/target_tracking/evaluate_solver_objectives.jl
+++ example/target_tracking/evaluate_solver_objectives.jl
@@ -10,31 +10,32 @@ using Printf
 
 close("all")
 
-println("Loading rollout library")
-library_file = "./data/rollout_library.jld2"
-@time @load library_file data num_robots trials
-rollouts = data
+trials = 1:10
 
-num_robots = 10
+experiment_name = "evaluate_solver_objectives"
+data_folder = "./data"
 
-# Create a map from number of robots to a vector of configs sample pairs
-samples = vcat(map(trials) do trial
-                 rollout = rollouts[num_robots, trial]
-
-                 [(configs=rollout.configs, sample=sample)
-                  for sample in rollout.samples]
-               end...)
-
-# Everything has the same number of samples
-sample_inds = 1:length(samples)
-
-data_file = "./data/evaluate_solver_objectives.jld2"
 reprocess = false
+
+# Values taken from the entropy_by_solver script
+num_robots = 16
+trials = 1:10
+solver_ind = 4 # dist-4
+
+# Drop the first n and keep every few after
+sample_inds = 20:5:100
+
+library_solver_ind = 4 # dist-4
 
 horizon = SubmodularMaximization.default_horizon
 
+# We have to override the number of samples used to generate the library
+num_mcts_samples = SubmodularMaximization.default_num_iterations[horizon]
+
+
 solver_rounds = [2, 4, 8]
-solvers = [solve_myopic,
+solvers = [solver_random,
+           solve_myopic,
            map(num->prob->solve_n_partitions(num, prob), solver_rounds)...,
            solve_sequential]
 solver_strings = ["myopic",
@@ -42,74 +43,73 @@ solver_strings = ["myopic",
                   "sequential"]
 solver_inds = 1:length(solvers)
 
-all_tests = product(solver_inds, sample_inds)
-all_configurations = product(solver_inds)
+#
+# Load prior trials
+#
 
-# We have to override the number of samples used to generate the library
-num_mcts_samples = SubmodularMaximization.default_num_iterations[horizon]
+println("Loading rollouts")
+library_file = "./data/entropy_by_solver.jld2"
 
-function get_data()
-  results = Array{Float64}(undef, size(all_tests))
-
-  num_tests_completed = Atomic{Int64}(0)
-
-  if !isfile(data_file) || reprocess
-
-    start = time()
-    iters = collect(all_tests)
-    @threads for (solver_ind, sample_ind) in iters
-      trial_start = time()
-
-      test = samples[sample_ind]
-
-      sample = test.sample
-
-      configs = MultiRobotTargetTrackingConfigs(test.configs;
-                                          horizon = horizon,
-                                          solver_iterations = num_mcts_samples)
-      problem = MultiRobotTargetTrackingProblem(sample.robot_states,
-                                                sample.target_filters,
-                                                configs)
-      solution = solvers[solver_ind](problem)
-      reward = solution.value
-
-      results[solver_ind, sample_ind] = reward
-
-      elapsed = time() - start
-      trial_elapsed = time() - trial_start
-
-      # (returns old value)
-      completed = atomic_add!(num_tests_completed, 1) + 1
-      completion = completed/length(all_tests)
-      projected = elapsed / completion
-      hour = 3600
-
-      println("Solver: ", solver_strings[solver_ind],
-              " Trial: ", sample_ind)
-      println(" (Done, ", completed, "/", length(all_tests),
-              @sprintf(" %0.2f", 100completed/length(all_tests)), "%",
-              " Trial time: ", @sprintf("%0.0fs", trial_elapsed),
-              " Elapsed: ", @sprintf("%0.0fs", elapsed),
-              " Total: ", @sprintf("%0.2fh", projected / hour),
-              " Remaining: ", @sprintf("%0.2fh", (projected - elapsed) / hour),
-              ")"
-             )
-    end
-
-    @save data_file results
-  else
-    @load data_file results
-  end
-
-  results
+let
+  @load library_file results
+  global library_results = results
 end
+
+# Create a map from number of robots to a vector of configs sample pairs
+samples = vcat(map(trials) do trial
+                 rollout = library_results[library_solver_ind, num_robots, trial]
+
+                 [(configs=rollout.configs, sample=sample)
+                  for sample in rollout.data[sample_inds]]
+               end...)
+
+
+#
+# Evaluate results on solvers
+#
+
+all_tests = product(solver_inds, sample_inds)
+
+function trial_fun(x)
+  solver_ind, sample_ind = x
+
+  test = samples[sample_ind]
+
+  sample = test.sample
+  configs = MultiRobotTargetTrackingConfigs(test.configs;
+                                            horizon = horizon,
+                                            solver_iterations = num_mcts_samples)
+
+  problem = MultiRobotTargetTrackingProblem(sample.robot_states,
+                                            sample.target_filters,
+                                            configs)
+  solution = solvers[solver_ind](problem)
+
+  solution.value
+end
+function print_summary(x)
+  solver_ind, sample_ind = x
+
+  println("Solver: ", solver_strings[solver_ind],
+          " Sample: ", sample_ind)
+end
+
+@time results = run_experiments(all_tests,
+                                trial_fun=trial_fun,
+                                print_summary=print_summary,
+                                experiment_name=experiment_name,
+                                data_folder=data_folder,
+                                reprocess=reprocess
+                               )
 
 println("Processing rollouts")
 @time results = get_data()
 
 println("Analyzing results")
 
-boxplot(results', notch=false, vert=false)
+results_matrix = map(x->results[x], all_tests)
+boxplot(results_matrix', notch=false, vert=false)
+
 title("Solver Objective Performance")
 xlabel("Mutual Information Objective (bits)")
 yticks(solver_inds, solver_strings)
