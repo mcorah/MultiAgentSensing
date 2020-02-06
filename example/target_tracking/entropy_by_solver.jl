c8ed2bdf137e71e5588c8641840bbdb13a3f3aa6
Micah Corah <micahcorah@gmail.com>
2020-02-06 16:58:17 -0500

    updating entropy by solver script and new experiments

--- example/target_tracking/entropy_by_solver.jl
+++ example/target_tracking/entropy_by_solver.jl
@@ -10,13 +10,12 @@ using Random
 close("all")
 
 experiment_name = "entropy_by_solver"
-data_file = string("./data/", experiment_name, ".jld2")
-cache_folder = string("./data/", experiment_name, "/")
+data_folder = "./data"
 
 reprocess = false
 
 steps = 100
-num_robots = [4, 8, 12, 16]
+num_robots = 8:8:40
 
 trials = 1:10
 
@@ -38,122 +37,37 @@ solver_strings = ["random",
                   "sequential"]
 solver_inds = 1:length(solvers)
 
-
 all_tests = product(solver_inds, num_robots, trials)
 all_configurations = product(solver_inds, num_robots)
 
-function get_data()
-  results = Dict{Any,Any}(key=>nothing for key in all_tests)
 
-  load_save_lock = SpinLock()
+function trial_fun(x)
+  (solver_ind, num_robots, trial) = x
 
-  if !isfile(data_file) || reprocess
-    if !isdir(cache_folder)
-      mkdir(cache_folder)
-    end
+  configs = MultiRobotTargetTrackingConfigs(num_robots, horizon=horizon)
 
-    num_tests_completed = Atomic{Int64}(0)
+  trial_data = target_tracking_experiment(steps=steps,
+                                          num_robots=num_robots,
+                                          configs=configs,
+                                          solver=solvers[solver_ind]
+                                         )
+  (data=trial_data, configs=configs)
+end
+function print_summary(x)
+  (solver_ind, num_robots, trial) = x
 
-    start = time()
-    iters = shuffle!(collect(all_tests))
-
-    # Error, protected by mutex
-    error = nothing
-
-    @threads for trial_spec in iters
-      trial_start = time()
-
-      local trial_data, configs
-      trial_file = string(cache_folder, experiment_name, " ", trial_spec, ".jld2")
-
-      (solver_ind, num_robots, trial) = trial_spec
-
-      # Cache data from each trial
-      if !isfile(trial_file) || reprocess
-        println("Thread-", threadid(), " running: ", trial_spec)
-
-        configs = MultiRobotTargetTrackingConfigs(num_robots,
-                                                  horizon=horizon,
-                                                 )
-
-        trial_data = target_tracking_experiment(steps=steps,
-                                                num_robots=num_robots,
-                                                configs=configs,
-                                                solver=solvers[solver_ind]
-                                               )
-        try
-          lock(load_save_lock)
-          print(threadid(), "-Saving: ", trial_spec, "...")
-
-          @save trial_file trial_data configs
-        catch e
-          # Save the error for later
-          error = e
-
-          println(threadid(), "-Failed to save ", trial_spec)
-          continue
-        finally
-          println(threadid(), "-Saved")
-          unlock(load_save_lock)
-        end
-      else
-        try
-          lock(load_save_lock)
-          print(threadid(), "-Loading: ", trial_file, "...")
-
-          @load trial_file trial_data configs
-          println(threadid(), "-Loaded")
-        catch e
-          # Save the error for later
-          error = e
-
-          println(threadid(), "-Failed to load ", trial_spec)
-          continue
-        finally
-          unlock(load_save_lock)
-        end
-      end
-
-      elapsed = time() - start
-      trial_elapsed = time() - trial_start
-
-      results[trial_spec] = (data=trial_data, configs=configs)
-
-      # (returns old value)
-      completed = atomic_add!(num_tests_completed, 1) + 1
-      completion = completed/length(all_tests)
-      projected = elapsed / completion
-      hour = 3600
-
-      lock(load_save_lock)
-      println("Num. Robots: ", num_robots,
-              " Solver: ", solver_strings[solver_ind],
-              " Trial: ", trial)
-      println(" (Done, ", completed, "/", length(all_tests),
-              @sprintf(" %0.2f", 100completion), "%",
-              " Trial time: ", @sprintf("%0.0fs", trial_elapsed),
-              " Elapsed: ", @sprintf("%0.2fh", elapsed / hour),
-              " Total: ", @sprintf("%0.2fh", projected / hour),
-              " Remaining: ", @sprintf("%0.2fh", (projected - elapsed) / hour),
-              ")"
-             )
-      unlock(load_save_lock)
-    end
-
-    if !isnothing(error)
-      println("Loading or producing results produce errors. Rethrowing.")
-      rethrow(error)
-    end
-
-    @save data_file results
-  else
-    @load data_file results
-  end
-
-  results
+  println("Num. Robots: ", num_robots,
+          " Solver: ", solver_strings[solver_ind],
+          " Trial: ", trial)
 end
 
-@time results = get_data()
+@time results = run_experiments(all_tests,
+                             trial_fun=trial_fun,
+                             print_summary=print_summary,
+                             experiment_name=experiment_name,
+                             data_folder=data_folder,
+                             reprocess=reprocess
+                            )
 
 # Normalize by number of targets
 entropies = Dict(map(all_tests) do key
