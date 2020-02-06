7ca62337b61b420a798f46486304c1c2d9e598d6
Micah Corah <micahcorah@gmail.com>
2020-02-06 16:21:02 -0500

    updating the solver conergence script

--- example/target_tracking/plot_convergence_by_exploration_constant.jl
+++ example/target_tracking/plot_convergence_by_exploration_constant.jl
@@ -10,81 +10,74 @@ using JLD2
 
 close()
 
-data_file = "./data/mcts_exploration_constant_data.jld2"
+data_folder = "./data"
+experiment_name = "mcts_exploration_constant_data"
 reprocess = false
 
-num_trials = 100
-num_mcts_samples = 2 .^ (4:16)
+num_trials = 1:100
+num_mcts_samples = map(x->round(Int64, x), 1.3 .^ (15:26))
 
 horizon = SubmodularMaximization.default_horizon
 
-exploration_constants = 1.6 .* [0.01, 0.1, 0.5, 1.0, 5.0, 10.0, 100.0]
+exploration_constants =  1.03 .* [0.1, 0.5, 0.7, 1.0, 1.3, 1.5, 10.0]
 
-grid_size = 10
-sensor = RangingSensor()
+grid_size = 12
+const sensor = RangingSensor()
 
 # place the target in the center of the grid
-grid = Grid(grid_size, grid_size)
-target_state = (5,5)
-robot_state = (1,1)
+const grid = Grid(grid_size, grid_size)
+const target_state = (8,8)
+const robot_state = (3,3)
 
 # Create some uncertainty. Two steps is probably a reasonable approximation of
 # the kind of target uncertainty that we will encounter
-histogram_filter = Filter(grid, target_state)
+const histogram_filter = Filter(grid, target_state)
 process_update!(histogram_filter, transition_matrix(grid))
 
-function get_data()
-  data = Array{Float64}(undef, num_trials,
-                               length(num_mcts_samples),
-                               length(exploration_constants))
-
-  if !isfile(data_file) || reprocess
-    for (kk, exploration_constant) in enumerate(exploration_constants),
-      (jj, num_mcts_samples) in Iterators.reverse(enumerate(num_mcts_samples))
-      println("Exploration constant: ", exploration_constant,
-              " Num. samples: ", num_mcts_samples)
-
-      problem = SingleRobotTargetTrackingProblem(grid, sensor, horizon,
+const problem = SingleRobotTargetTrackingProblem(grid, sensor, horizon,
                                                  [histogram_filter])
 
-      @time for (block_num, block) in enumerate(partition(1:num_trials,
-                                                          Threads.nthreads()))
-        println("Block num: ", block_num)
-        # Ensure that thread execution does not overlap
-        @threads for ii in block
-          # solve via mcts
-          trajectory = solve_single_robot(problem, robot_state,
-                                          n_iterations = num_mcts_samples,
-                                          exploration_constant=
-                                            exploration_constant
-                                         ).trajectory
+all_tests = product(exploration_constants, num_mcts_samples, num_trials)
+all_configurations = product(exploration_constants, num_mcts_samples)
 
-          # evaluate mutual information using the default number of samples (which
-          # should be quite accurate)
-          reward = finite_horizon_information(grid, histogram_filter, sensor,
-                                              trajectory).reward
+function trial_fun(x)
+  (exploration_constant, num_mcts_samples, trial) = x
 
-          data[ii, jj, kk] = reward
-        end
-      end
-    end
+  trajectory = solve_single_robot(problem, robot_state,
+                                  n_iterations=num_mcts_samples,
+                                  exploration_constant=exploration_constant
+                                 ).trajectory
 
-    @save data_file data
-  else
-    @load data_file data
-  end
+  # evaluate mutual information using the default number of samples (which
+  # should be quite accurate)
+  finite_horizon_information(grid, histogram_filter, sensor,
+                             trajectory).reward
+end
+function print_summary(x)
+  (exploration_constant, num_mcts_samples, trial) = x
 
-  data
+  println("Exploration constant: ", exploration_constant,
+          " Num. samples: ", num_mcts_samples,
+          " Trial: ", trial,
+         )
 end
 
-data = get_data()
+@time data = run_experiments(all_tests,
+                             trial_fun=trial_fun,
+                             print_summary=print_summary,
+                             experiment_name=experiment_name,
+                             data_folder=data_folder,
+                             reprocess=reprocess,
+                            )
 
-mean_rewards = reshape(mean(data, dims=1), length.((num_mcts_samples,
-                                                    exploration_constants)))
+mean_rewards = map(all_configurations) do configuration
+  mean(x->data[configuration..., x], num_trials)
+end
+
 maximum_reward = maximum(mean_rewards)
 
 for (ii, exploration_constant) in enumerate(exploration_constants)
-  data = mean_rewards[:,ii]
+  data = mean_rewards[ii,:]
   fraction_rewards =  data ./ maximum_reward
 
   label = string("Exp. const.: ", exploration_constant)
