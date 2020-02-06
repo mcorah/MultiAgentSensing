598d30fe3a72318d7fcc125e46a0e00b8720f2ac
Micah Corah <micahcorah@gmail.com>
2020-02-06 17:14:55 -0500

    rewriting weights by number of robots script

--- example/target_tracking/weights_by_number_of_robots.jl
+++ example/target_tracking/weights_by_number_of_robots.jl
@@ -18,15 +18,17 @@ cache_folder = string("./data/", experiment_name, "/")
 
 reprocess = false
 
-
-
 # Values taken from the entropy_by_solver script
-num_robots = [4, 8, 12, 16]
+num_robots = 8:8:40
 trials = 1:10
-solver_ind = 6 # sequential
-trial_steps = 20:100
+solver_ind = 4 # dist-4
 
-# load and preprocess the entropy results
+# Drop the first n and keep every few after
+trial_steps = 20:5:100
+
+#
+# Load and preprocess prior entropy results
+#
 
 solver_results_file = "./data/entropy_by_solver.jld2"
 
@@ -43,136 +45,49 @@ weights_trials = Dict(map(all_tests) do test
   test => solver_results[solver_ind, test...]
 end)
 
-function get_data()
-  results = Dict{Any,Any}(key=>nothing for key in all_tests)
+#
+# Compute weights
+#
 
-  if !isfile(data_file) || reprocess
-    if !isdir(cache_folder)
-      mkdir(cache_folder)
-    end
+function trial_fun(x)
+  num_robots, trial = trial_spec
 
-    num_tests_completed = Atomic{Int64}(0)
+  # The nomenclature here is weird, but we will only be using a part of
+  # the trial and will implement that at computation time.
+  data, configs = weights_trials[trial_spec...]
+  trial_data = data[trial_steps]
 
-    # First process any data that has been saved
-    remaining_tests = filter(collect(all_tests)) do trial_spec
-      run_test = true
+  # Iterate through the trial and compute weights
+  trial_weights = map(trial_data) do step_data
+    robot_states = step_data.robot_states
+    target_filters = step_data.target_filters
 
-      trial_file = string(cache_folder, experiment_name, " ", trial_spec, ".jld2")
+    problem = MultiRobotTargetTrackingProblem(robot_states, target_filters,
+                                              configs)
 
-      # Cache data from each trial
-      if !reprocess && isfile(trial_file)
-        try
-          print(threadid(), "-Loading: ", trial_file, "...")
-
-          @load trial_file trial_weights trial_data configs
-          println(threadid(), "-Loaded")
-
-          results[trial_spec] = (
-                                 trial_weights=trial_weights,
-                                 trial_data=trial_data,
-                                 configs=configs
-                                )
-          run_test = false
-        catch e
-          println(threadid(), "-Failed to load ", trial_spec)
-        end
-      end
-
-      run_test
-    end
-
-    println("\n", length(remaining_tests), " tests remain\n")
-
-    # Mutex for load/save/output
-    load_save_lock = SpinLock()
-
-    start = time()
-    shuffle!(remaining_tests)
-    trial_error = nothing
-    @threads for trial_spec in remaining_tests
-      println("Thread-", threadid(), " running: ", trial_spec)
-
-      trial_start = time()
-
-      num_robots, trial = trial_spec
-
-      trial_file = string(cache_folder, experiment_name, " ", trial_spec, ".jld2")
-
-      # The nomenclature here is weird, but we will only be using a part of
-      # the trial and will implement that at computation time.
-      data, configs = weights_trials[trial_spec...]
-      trial_data = data[trial_steps]
-
-      # Iterate through the trial and compute weights
-      trial_weights = map(trial_data) do step_data
-        robot_states = step_data.robot_states
-        target_filters = step_data.target_filters
-
-        problem = MultiRobotTargetTrackingProblem(robot_states, target_filters,
-                                                  configs)
-
-        compute_weight_matrix(problem)
-      end
-
-      try
-        lock(load_save_lock)
-        print(threadid(), "-Saving: ", trial_spec, "...")
-
-        @save trial_file trial_weights trial_data configs
-      catch e
-        # Save the error for later
-        trial_error = e
-
-        println(threadid(), "-Failed to save ", trial_spec)
-        continue
-      finally
-        println(threadid(), "-Saved")
-        unlock(load_save_lock)
-      end
-
-      elapsed = time() - start
-      trial_elapsed = time() - trial_start
-
-      results[trial_spec] = (
-                             trial_weights=trial_weights,
-                             trial_data=trial_data,
-                             configs=configs
-                            )
-
-      # (returns old value)
-      completed = atomic_add!(num_tests_completed, 1) + 1
-      completion = completed/length(remaining_tests)
-      projected = elapsed / completion
-      hour = 3600
-
-      lock(load_save_lock)
-      println("Num. Robots: ", num_robots,
-              " Trial: ", trial)
-      println(" (Done, ", completed, "/", length(remaining_tests),
-              @sprintf(" %0.2f", 100completion), "%",
-              " Trial time: ", @sprintf("%0.0fs", trial_elapsed),
-              " Elapsed: ", @sprintf("%0.2fh", elapsed / hour),
-              " Total: ", @sprintf("%0.2fh", projected / hour),
-              " Remaining: ", @sprintf("%0.2fh", (projected - elapsed) / hour),
-              ")"
-             )
-      unlock(load_save_lock)
-    end
-
-    if !isnothing(trial_error)
-      println("Loading or producing results produce errors.")
-      error("One or more trials failed.")
-    end
-
-    @save data_file results
-  else
-    @load data_file results
+    compute_weight_matrix(problem)
   end
 
-  results
+  (trial_weights=trial_weights, trial_data=trial_data, configs=configs)
+end
+function print_summary(x)
+  num_robots, trial = trial_spec
+
+  println("Num. Robots: ", num_robots,
+          " Trial: ", trial)
 end
 
-@time results = get_data()
+@time results = run_experiments(all_tests,
+                                trial_fun=trial_fun,
+                                print_summary=print_summary,
+                                experiment_name=experiment_name,
+                                data_folder=data_folder,
+                                reprocess=reprocess
+                               )
+
+#
+# Plot weights by objective values
+#
 
 normalized_weights = map(num_robots) do num_robots
   trial_weights = map(trials) do trial
