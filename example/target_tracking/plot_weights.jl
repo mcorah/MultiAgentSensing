96c9db08504b56e5c23d338981f6210c54476424
Micah Corah <micahcorah@gmail.com>
2020-02-05 22:48:58 -0500

    revamping plot weights script to show multiples

--- example/target_tracking/plot_weights.jl
+++ example/target_tracking/plot_weights.jl
@@ -4,49 +4,56 @@
 using SubmodularMaximization
 using PyPlot
 
-close()
+close("all")
 
 grid_size = 41
 horizon = SubmodularMaximization.default_horizon
-sensor = RangingSensor()
 
-# place the target in the center of the grid
-target_state = (21,1)
+scaling_factors = [0.1, 0.5, 1.0, 2.0, 3.0]
 
-grid = Grid(grid_size, grid_size)
+for scaling_factor in scaling_factors
+  sensor = RangingSensor(variance_scaling_factor=scaling_factor)
 
-# Create some uncertainty. Two steps is probably a reasonable approximation of
-# the kind of target uncertainty that we will encounter
-histogram_filter = Filter(grid, target_state)
-process_update!(histogram_filter, transition_matrix(grid))
+  # place the target in the center of the grid
+  target_state = (21,1)
 
-range = 1:grid_size
+  grid = Grid(grid_size, grid_size)
 
-@time weights = thread_map(range) do x
-  state = (x, 1)
+  # Create some uncertainty. Two steps is probably a reasonable approximation of
+  # the kind of target uncertainty that we will encounter
+  histogram_filter = Filter(grid, target_state)
+  process_update!(histogram_filter, transition_matrix(grid))
 
-  problem = MultiRobotTargetTrackingProblem([state, state],
-                                            [histogram_filter],
-                                            grid=grid,
-                                            horizon=horizon)
+  range = 1:grid_size
 
-  weights = compute_weight_matrix(problem,
-                                  channel_capacity_method=
-                                    channel_capacities_mcts)
+  @time weights = thread_map(range) do x
+    state = (x, 1)
 
-  sum(weights) / 2
+    problem = MultiRobotTargetTrackingProblem([state, state],
+                                              [histogram_filter],
+                                              grid=grid,
+                                              sensor=sensor,
+                                              horizon=horizon)
+
+    weights = compute_weight_matrix(problem)
+
+    sum(weights) / 2
+  end
+
+  label = string("Scal. fact.: ", scaling_factor)
+  plot(range, weights, label=label)
+
+  ratio = maximum(weights)/minimum(weights)
+
+  println("Scaling factor: ", scaling_factor, " Weights falloff ratio: ", ratio)
 end
 
-plot(range, weights)
+plot_title = string("Weights Falloff")
 
-ratio = maximum(weights)/minimum(weights)
-println("Weights falloff ratio: ", ratio)
-
-plot_title = string("Weight Falloff (constant: ", sensor.variance_constant,
-                    ", scaling: ", sensor.variance_scaling_factor, ")")
 title(plot_title)
 xlabel("X position")
 ylabel("Weight (bits)")
+legend()
 
 save_fig("fig", plot_title)
 
