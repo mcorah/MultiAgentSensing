457ebf62f6d8a2414921e2ea7735482c2365b403
Micah Corah <micahcorah@gmail.com>
2020-05-11 18:44:20 -0400

    updating large scale weights to plot multiple traces nicely

--- example/target_tracking/large_scale_weights.jl
+++ example/target_tracking/large_scale_weights.jl
@@ -31,9 +31,22 @@ solver = x->solve_communication_range_limit(x, num_partitions=4,
                                             communication_range=
                                             communication_range)
 
+# Plotting configs
+
+line_styles = ["-", "--"]
+experiment_names = ["large-scale", "solver-trials"]
+
+# Use colors from the default cycle
+colors = get_cmap("tab10")
+weights_color = colors(0)
+objective_color = colors(1)
+
 #
 # Code to run experiments
 #
+
+# We will also load a separate set of results from the experiments
+results_array = []
 
 all_tests = product(num_robots, trials)
 all_configurations = num_robots
@@ -76,14 +89,36 @@ function print_summary(x)
           " Trial: ", trial)
 end
 
-@time results = run_experiments(all_tests,
-                                trial_fun=trial_fun,
-                                print_summary=print_summary,
-                                experiment_name=experiment_name,
-                                data_folder=data_folder,
-                                reprocess=reprocess,
-                                threaded=true
-                               )
+let
+  @time results = run_experiments(all_tests,
+                                  trial_fun=trial_fun,
+                                  print_summary=print_summary,
+                                  experiment_name=experiment_name,
+                                  data_folder=data_folder,
+                                  reprocess=reprocess,
+                                  threaded=true
+                                 )
+  push!(results_array, (results, num_robots, trials))
+end
+
+#
+# *Optionally* load the weights dataset from the full set of trials if the data
+# exists
+#
+
+experiment_weights_file = string(data_folder,
+                                 "/weights_by_number_of_robots.jld2")
+
+let
+  if isfile(experiment_weights_file)
+    @load experiment_weights_file results
+
+    num_robots = sort(union([first(x) for x in keys(results)]))
+    trials = sort(union([last(x) for x in keys(results)]))
+
+    push!(results_array, (results, num_robots, trials))
+  end
+end
 
 #
 # Weights per num robots
@@ -91,28 +126,33 @@ end
 
 figure()
 
-weight_per_robot = map(num_robots) do num_robots
-  trial_weights = map(trials) do trial
-    trial = results[num_robots, trial]
+for ii in 1:length(results_array)
+  (results, num_robots, trials) = results_array[ii]
+  style = line_styles[ii]
+  experiment_name = experiment_names[ii]
 
-    trial_weights = trial.trial_weights
+  weight_per_robot = map(num_robots) do num_robots
+    trial_weights = map(trials) do trial
+      trial = results[num_robots, trial]
 
-    total_weights = map(total_weight, trial_weights)
+      trial_weights = trial.trial_weights
 
-    total_weights / num_robots
+      total_weights = map(total_weight, trial_weights)
+
+      total_weights / num_robots
+    end
+
+    vcat(trial_weights...)
   end
 
-  vcat(trial_weights...)
+  weights_series = TimeSeries(num_robots, hcat(weight_per_robot...)')
+  plot_trials(weights_series, mean=true, marker=".", markersize=25, linewidth=3,
+              color=weights_color, linestyle=style,
+              label=string("redundancy, ", experiment_name))
 end
 
-weights_color=:blue
-
-weights_series = TimeSeries(num_robots, hcat(weight_per_robot...)')
-plot_trials(weights_series, mean=true, marker=".", markersize=25, linewidth=3,
-            color=weights_color, label="weights")
-
 tick_params(axis=:y, labelcolor=weights_color)
-ylabel("Cost bound per num. robots", color=weights_color)
+ylabel("Redundancy per robot", color=weights_color)
 
 weights_axis = gca()
 
@@ -122,26 +162,31 @@ weights_axis = gca()
 
 twinx()
 
-objective_values = map(num_robots) do num_robots
-  trial_objectives = map(trials) do trial
-    trial = results[num_robots, trial]
+for ii in 1:length(results_array)
+  (results, num_robots, trials) = results_array[ii]
+  style = line_styles[ii]
+  experiment_name = experiment_names[ii]
 
-    objectives = map(x->x.objective, trial.trial_data)
+  objective_values = map(num_robots) do num_robots
+    trial_objectives = map(trials) do trial
+      trial = results[num_robots, trial]
 
-    objectives / num_robots
+      objectives = map(x->x.objective, trial.trial_data)
+
+      objectives / num_robots
+    end
+
+    vcat(trial_objectives...)
   end
 
-  vcat(trial_objectives...)
+  objective_series = TimeSeries(num_robots, hcat(objective_values...)')
+  plot_trials(objective_series, mean=true, marker=".", markersize=25,
+              linewidth=3, color=objective_color, linestyle=style,
+              label=string("objective, ", experiment_name))
 end
 
-objective_color=:red
-
-objective_series = TimeSeries(num_robots, hcat(objective_values...)')
-plot_trials(objective_series, mean=true, marker=".", markersize=25, linewidth=3,
-            color=objective_color, label="objective")
-
 tick_params(axis=:y, labelcolor=objective_color)
-ylabel("Objective value per num. robots", color=objective_color)
+ylabel("Objective value per robot", color=objective_color)
 
 objective_axis = gca()
 
