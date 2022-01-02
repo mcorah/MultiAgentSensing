af257a6aaa8572f051d1f91ba9586758c14ccbe7
Micah Corah <micahcorah@gmail.com>
2022-01-02 15:14:16 -0800

    mostly empty coverage problem file definition

---
+++ src/target_tracking/multi_robot_target_tracking_problem.jl
@@ -0,0 +1,7 @@
+# This file provides most of the implementation of multi-robot target *tracking*
+# problems.
+#
+# The tracking problems involve estimation of target positions and optimization
+# via maximizing mutual information.
+
+using LinearAlgebra
