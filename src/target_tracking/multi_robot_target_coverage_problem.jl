b8dc8a2b134be1064655124706b0b9d3a55b56a9
Micah Corah <micahcorah@gmail.com>
2022-01-03 16:49:29 -0800

    body of the multi-robot coverage implementation

--- src/target_tracking/multi_robot_target_coverage_problem.jl
+++ src/target_tracking/multi_robot_target_coverage_problem.jl
@@ -8,3 +8,110 @@
 # estimations of position but otherwise seek to "cover" targets with sensors or
 # cameras.
 
+export MultiRobotTargetCoverageConfigs, MultiRobotTargetCoverageProblem
+
+# Automatically switch to sparse filters for large numbers of robots
+const num_robots_sparse_filtering_threshold = 15
+const sparsity_threshold=1e-3
+
+# Stores configuration variables for multi-robot target tracking
+struct MultiRobotTargetCoverageConfigs
+  grid::Grid
+  sensor::CoverageSensor
+  horizon::Int64
+
+  # Configuration for the MCTS solver
+  solver_iterations::Int64
+
+  robot_target_range_limit::Float64
+
+  function MultiRobotTargetCoverageConfigs(;
+                                           grid,
+                                           horizon=default_horizon,
+                                           sensor=CoverageSensor(),
+                                           solver_iterations =
+                                           default_num_iterations[horizon],
+                                           robot_target_range_limit = Inf
+                                          )
+
+    new(grid, sensor, horizon, solver_iterations, robot_target_range_limit)
+  end
+end
+
+# Constructor that can infer parameters based on the number of robots
+#
+# Note that inferred parameters can still be overriden, but we probably don't
+# want to do so
+function MultiRobotTargetCoverageConfigs(num_robots;
+                                         kwargs...)
+  grid=Grid(num_robots=num_robots)
+  MultiRobotTargetCoverageConfigs(;grid=grid, kwargs...)
+end
+
+# Copy constructor with overrides
+function
+  MultiRobotTargetCoverageConfigs(configs::MultiRobotTargetCoverageConfigs;
+                                  kwargs...)
+
+  fields = fieldnames(MultiRobotTargetCoverageConfigs)
+  old_settings = Dict(field => getfield(configs, field) for field in fields)
+  settings = merge(old_settings, kwargs)
+
+  MultiRobotTargetCoverageConfigs(; settings...)
+end
+
+# Solution elements consist of the robot index and the associated trajectory
+# We construct solutions as such because the output may not have the same
+# ordering as the robots
+#
+# Warning: this "problem" object may become invalid if any of the underlying
+# objects change and may copy some but not of the inputs.
+struct MultiRobotTargetCoverageProblem <: AbstractTargetProblem
+  # Target tracking problems are defined by vectors of robot states
+  partition_matroid::Vector{State}
+
+  target_states::Vector{State}
+
+  configs::MultiRobotTargetCoverageConfigs
+end
+
+# Construct a target coverage problem with configs.
+function MultiRobotTargetCoverageProblem(robot_states::Vector{State},
+                                         target_states::Vector{State};
+                                         kwargs...)
+  configs = MultiRobotTargetCoverageConfigs(;kwargs...)
+
+  MultiRobotTargetCoverageProblem(robot_states, target_states, configs)
+end
+
+function objective(p::MultiRobotTargetCoverageProblem, X)
+  configs = p.configs
+
+  trajectories = map(last, X)
+
+  sum(p.target_states) do target
+    finite_horizon_coverage(configs.grid, state, configs.sensor,
+                            trajectories;
+                           ).reward
+  end
+end
+
+function solve_block(p::MultiRobotTargetCoverageProblem, block::Integer,
+                     selections::Vector)
+  configs = p.configs
+
+  trajectories = map(last, selections)
+
+  problem = SingleRobotTargetCoverageProblem(configs.grid,
+                                             configs.sensor,
+                                             configs.horizon,
+                                             p.target_states,
+                                             prior_trajectories=trajectories)
+
+  state = get_state(p, block)
+
+  solution = solve_single_robot(problem, state,
+                                n_iterations=configs.solver_iterations)
+
+  (block, solution.trajectory)
+end
