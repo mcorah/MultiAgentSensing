17255174e6ee11fa90700b5175c651fc075a0734
Micah Corah <micahcorah@gmail.com>
2022-01-03 19:02:26 -0800

    Adding abstract single-robot problems in preparation for implementing a concrete coverage problem

--- src/target_tracking/single_robot_solver.jl
+++ src/target_tracking/single_robot_solver.jl
@@ -8,8 +8,8 @@ using Random
 import POMDPs.isterminal
 import POMDPs.actions
 
-export SingleRobotTargetTrackingProblem, MDPState, generate_solver,
-  solve_single_robot, isterminal
+export AbstractSingleRobotProblem, SingleRobotTargetTrackingProblem, MDPState,
+generate_solver, solve_single_robot, isterminal
 
 const default_solver_information_samples = 1
 
@@ -54,9 +54,13 @@ MDPState(state) = MDPState(state, 0, nothing)
 # Construct a new state in a trajectory
 MDPState(m::MDPState, s::State) = MDPState(s, m.depth + 1, m)
 
+# Abstract implementation of something like a single-robot tracking problem
+abstract type AbstractSingleRobotProblem <: MDP{MDPState, State}
+end
+
 # The target tracking problem is an mdp with MDPStates for states
 # And target tracking States for actions
-struct SingleRobotTargetTrackingProblem{F<:AnyFilter} <: MDP{MDPState, State}
+struct SingleRobotTargetTrackingProblem{F<:AnyFilter} <: AbstractSingleRobotProblem
   grid::Grid
   sensor::RangingSensor
   horizon::Int64
@@ -90,7 +94,7 @@ function generate_solver(depth;
 end
 
 # Return the next state for the robot
-function solve_single_robot(problem::SingleRobotTargetTrackingProblem,
+function solve_single_robot(problem::AbstractSingleRobotProblem,
                             state::State;
                             n_iterations =
                               default_num_iterations[problem.horizon],
@@ -113,7 +117,7 @@ function solve_single_robot(problem::SingleRobotTargetTrackingProblem,
 end
 
 # Pull the tree out of the info object
-function extract_trajectory(problem::SingleRobotTargetTrackingProblem,
+function extract_trajectory(problem::AbstractSingleRobotProblem,
                             tree::MCTS.MCTSTree,
                             initial_state::State)
 
@@ -140,21 +144,21 @@ function extract_trajectory(problem::SingleRobotTargetTrackingProblem,
   states
 end
 
-horizon(x::SingleRobotTargetTrackingProblem) = x.horizon
+horizon(x::AbstractSingleRobotProblem) = x.horizon
 
 # I probably dont need to define this, but it's good to have
-function isterminal(model::SingleRobotTargetTrackingProblem, state)
+function isterminal(model::AbstractSingleRobotProblem, state)
   state.depth == model.horizon
 end
 
-function actions(model::SingleRobotTargetTrackingProblem, state::MDPState)
+function actions(model::AbstractSingleRobotProblem, state::MDPState)
   neighbors(model.grid, state.state)
 end
-function actions(model::SingleRobotTargetTrackingProblem)
+function actions(model::AbstractSingleRobotProblem)
   println("providing all actions")
   get_states(model.grid)
 end
-POMDPs.discount(model::SingleRobotTargetTrackingProblem) = 1.0
+POMDPs.discount(model::AbstractSingleRobotProblem) = 1.0
 
 # This tree implementation should be fine since everything should already be in
 # cache once we reach the end.
@@ -195,7 +199,7 @@ end
 # Generative model for the single-robot MDP
 #
 # We will sample the reward once we reach the end of the trajectory
-function POMDPs.gen(model::SingleRobotTargetTrackingProblem,
+function POMDPs.gen(model::AbstractSingleRobotProblem,
                     s::MDPState, a::State, rng)
   new_state = MDPState(s, a)
   reward = 0.0
