0d73404f9e4fa7366e7597dd8565fbd41236657f
Micah Corah <micahcorah@gmail.com>
2022-01-03 16:41:47 -0800

    empty implementation of multi-robot coverage

---
+++ src/target_tracking/multi_robot_target_coverage_problem.jl
@@ -0,0 +1,10 @@
+# This file provides most of the implementation of multi-robot target *coverage*
+# problems.
+#
+# These coverage problems involve controlling robots to cover a number of
+# targets e.g. to keep them within range of onboard sensors.
+#
+# This problem is applicable to cinematography whereas robots may have good
+# estimations of position but otherwise seek to "cover" targets with sensors or
+# cameras.
+
